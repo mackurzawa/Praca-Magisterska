5c19f93d0e5e8d2151ed93d6895b357d76203b33
mackurzawa <mac.kurzawa@gmail.com>
2024-07-11 22:30:00 +0200

    CalculateMetrics optimized

--- Ender/CalculateMetrics.py
+++ Ender/CalculateMetrics.py
@@ -7,17 +7,9 @@ def calculate_accuracy(y, y_preds):
     return accuracy_score(y, y_pred_labels)
 
 
-
 def calculate_all_metrics(y, y_preds):
 
-    # accuracy = accuracy_score(y, y_pred_labels)
     accuracy = calculate_accuracy(y, y_preds)
-
-
-
-    # y_pred_for_true_labels = [y_preds[i][y[i]] for i in range(len(y))]
-    # perfect_probabilites = [1 for _ in y]
-    # mean_absolute_err = mean_absolute_error(perfect_probabilites, y_pred_for_true_labels)
     mean_absolute_err = None
 
     return {'accuracy': accuracy, 'mean_absolute_error': mean_absolute_err}
